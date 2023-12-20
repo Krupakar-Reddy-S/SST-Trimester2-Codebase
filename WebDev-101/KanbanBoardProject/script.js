a801f96de5c5a2c06b2b642646cf8c51b3de75ed
Krupakar Reddy <krupakarreddysuda0@gmail.com>
2023-12-20 11:02:24 +0530

    Class Code Update

--- WebDev-101/KanbanBoardProject/script.js
+++ WebDev-101/KanbanBoardProject/script.js
@@ -1,6 +1,10 @@
 let addBtn = document.querySelector('.add-btn');
-let modal = document.querySelector('.modal-cont')
+let modal = document.querySelector('.modal-cont');
+let textArea = document.querySelector('.textarea-cont');
+let mainCont = document.querySelector('.main-cont');
+let allPriorityColor = document.querySelectorAll('.priority-color');
 let addModal = true;
+let taskColor = 'red';
 
 addBtn.addEventListener('click',function(){
     console.log("Btn has been clicked")
@@ -11,3 +15,49 @@ addBtn.addEventListener('click',function(){
     }
     addModal = !addModal;
 })
+
+textArea.addEventListener('keydown',function(e){
+    // console.log(e);
+    let key = e.key;
+    if(key === "Enter"){
+        // console.log("Generate Ticket");
+        // console.log(textArea.value);
+        if(textArea.value == ""){
+            textArea.value = "";
+            alert("Please Enter some task!");
+            return;
+        }
+        generateTicket(textArea.value);
+        textArea.value = "";
+        modal.style.display = 'none'
+        addModal = true
+    }
+})
+
+for(let i=0;i<allPriorityColor.length;i++){
+    allPriorityColor[i].addEventListener("click",function(){
+        // console.log(allPriorityColor[i])
+        for(let j=0;j<allPriorityColor.length;j++){
+            allPriorityColor[j].classList.remove('active');
+        }
+        // console.log(allPriorityColor[i])
+        allPriorityColor[i].classList.add('active')
+        taskColor = allPriorityColor[i].classList[1];
+        console.log(taskColor)
+    })
+}
+
+function generateTicket(task){
+    // <div class="ticket-cont">
+        // <div class="ticket-color green"></div>
+        // <div class="ticket-id">#eidut3</div>
+        // <div class="ticket-area">Some Task</div>
+    // </div>
+    let ticketCont = document.createElement("div");
+    ticketCont.className = "ticket-cont";
+    ticketCont.innerHTML = `<div class="ticket-color ${taskColor}"></div>
+                            <div class="ticket-id">#eidut3</div>
+                            <div class="ticket-area">${task}</div>`
+    console.log(ticketCont)
+    mainCont.appendChild(ticketCont);
+}
