d689ff95e439ebc12a034c32ff8d68b1ac4f729d
Krupakar-Reddy-S <krupakarreddysuda0@gmail.com>
2024-01-17 12:10:16 +0530

    Cleaned up DS Class Files to remove Duplicates

---
+++ DSA-101/Trees1/TreesTest.java
@@ -0,0 +1,9 @@
+public class TreesTest {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(8);
+
+        System.out.println(root.val);
+    }
+}
